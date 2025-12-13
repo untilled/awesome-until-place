8804545b7c022bd0c15e9dac6bc19237d65c63cd
morethanmin <morethanmin.dev@gmail.com>
2025-12-13 20:47:59 +0900

    feat: update address format for 건축가의 서재 and add .env to .gitignore

--- scripts/fill-coordinates.js
+++ scripts/fill-coordinates.js
@@ -34,6 +34,10 @@ async function getCoordinatesFromAddress(address) {
 
         res.on('end', () => {
           try {
+            console.log('api success')
+
+            console.log(data)
+
             const json = JSON.parse(data)
 
             if (json.documents && json.documents.length > 0) {
